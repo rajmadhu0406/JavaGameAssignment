d260b142948b0a3f1e6d85b47b178c7baf870af9
rajmadhu0406-local <raj.madhu0406@gmail.com>
2022-12-19 01:16:48 +0530

    removed mylog.log file

--- src/main/java/com/raj/natwest/service/GameServiceImpl.java
+++ src/main/java/com/raj/natwest/service/GameServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.FileHandler;
 
 @Service
 public class GameServiceImpl implements GameService {
